a9894ffff66734cac630ac9f62e40b45f1784323
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-09-25 13:30:30 +1200

    ready to deploy actually this time

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -18,9 +18,7 @@ class Homepage extends React.Component {
   }
 
   componentDidMount() {
-
     api.getAllAnimals(this.getAnimalsByParam)
-   
   }
 
   render() {
@@ -31,7 +29,7 @@ class Homepage extends React.Component {
           <div className="animal-home-display">
               {this.state.animals.map(animal => {
                 arr.push(animal)
-                return arr.length < 6 ? (<div className="homepage-img">
+                return arr.length <= 6 ? (<div className="homepage-img">
                   <img src={animal.imgSrc} alt={animal.type} onClick={() => {this.props.setActivePage("species", animal.type)}}/>
                   <h3>{animal.type}</h3>
                 </div>) : null
